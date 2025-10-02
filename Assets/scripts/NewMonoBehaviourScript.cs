f4ee6e081519df0a9342c09d8dd39b751998d8d3
Chehpa <martin.roudaut44@gmail.com>
2025-10-02 17:23:00 -0400

    1002

--- Assets/scripts/NewMonoBehaviourScript.cs
+++ Assets/scripts/NewMonoBehaviourScript.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    //public LayerMask wallLayer;
+    public Transform Wall_R_Check;
+    public Transform Wall_L_Check;
 
     private Rigidbody2D rb;
     private Vector2 movement;
+    private bool isGrounded;
+
+    public float GizSize = 0.1f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,6 +28,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        Debug.Log(isGrounded);
 
     }
     void FixedUpdate()
